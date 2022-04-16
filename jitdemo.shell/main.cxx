8523ea47ea2c7837d2d89257c3bd6a8bb71363fe
Chanjung Kim <freiyer.paxbun@gmail.com>
2022-04-16 13:30:55 +0900

    Add examples in jitdemo.shell.main

--- jitdemo.shell/main.cxx
+++ jitdemo.shell/main.cxx
@@ -1,6 +1,17 @@
 // Copyright (c) 2022 Chanjung Kim. All rights reserved.
 
-import jitdemo.expr;
-import jitdemo.jit;
+//import jitdemo.expr;
+//
+//using ::jitdemo::expr::Context;
+//using ::jitdemo::expr::Parse;
+//using ::jitdemo::expr::Tokenize;
 
-int main(int argc, char** argv) {}
+int main(int argc, char** argv)
+{
+    //Context context;
+    //auto    source { u8"f(x, y) = x^3 / x * sin(x) + .5 * log(x, 10.2) -  y   " };
+    //auto    result { Tokenize(source) };
+    //auto    parseResult { Parse(context, result.tokens) };
+
+    return 0;
+}
