a429c2dee670d5ba4054a4b5d55d600f2daebcec
Chanjung Kim <freiyer.paxbun@gmail.com>
2022-04-25 09:47:09 +0900

    Update sample code in main.cxx

--- jitdemo.shell/main.cxx
+++ jitdemo.shell/main.cxx
@@ -1,5 +1,7 @@
 // Copyright (c) 2022 Chanjung Kim. All rights reserved.
 
+#include <cmath>
+#include <iomanip>
 #include <iostream>
 #include <span>
 #include <vector>
@@ -18,7 +20,14 @@ int main(int argc, char** argv)
     auto    parseResult { Parse(context, result.tokens) };
 
     double params[] { 1.3, 4.6 };
-    ::std::cout << parseResult.function->Evaluate(params);
+    double expectedValue {
+        ::std::pow(params[0], 3) / params[0] * ::std::sin(params[0])
+            + .5 * ::std::log(10.2) / ::std::log(params[0]) - params[1],
+    };
+    double value { parseResult.function->Evaluate(params) };
+    ::std::cout << "Expected: " << ::std::setprecision(8) << expectedValue << '\n';
+    ::std::cout << "Actual: " << std::setprecision(8) << value << '\n';
+    ::std::cout << "Ok: " << ::std::boolalpha << (::std::abs(value - 1.45429756154) < 1e-8) << '\n';
 
     return 0;
 }
