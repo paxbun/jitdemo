1cba91c7210e54770f0a4ba77279edff39147979
Chanjung Kim <freiyer.paxbun@gmail.com>
2022-04-25 09:40:55 +0900

    Add sample code in main.cxx

--- jitdemo.shell/main.cxx
+++ jitdemo.shell/main.cxx
@@ -1,17 +1,24 @@
 // Copyright (c) 2022 Chanjung Kim. All rights reserved.
 
-//import jitdemo.expr;
-//
-//using ::jitdemo::expr::Context;
-//using ::jitdemo::expr::Parse;
-//using ::jitdemo::expr::Tokenize;
+#include <iostream>
+#include <span>
+#include <vector>
+
+import jitdemo.expr;
+
+using ::jitdemo::expr::Context;
+using ::jitdemo::expr::parsing::Parse;
+using ::jitdemo::expr::parsing::Tokenize;
 
 int main(int argc, char** argv)
 {
-    //Context context;
-    //auto    source { u8"f(x, y) = x^3 / x * sin(x) + .5 * log(x, 10.2) -  y   " };
-    //auto    result { Tokenize(source) };
-    //auto    parseResult { Parse(context, result.tokens) };
+    Context context;
+    auto    source { u8"f(x, y) = x^3 / x * sin(x) + .5 * log(x, 10.2) -  y   " };
+    auto    result { Tokenize(source) };
+    auto    parseResult { Parse(context, result.tokens) };
+
+    double params[] { 1.3, 4.6 };
+    ::std::cout << parseResult.function->Evaluate(params);
 
     return 0;
 }
