3dcfae071e66f000d7bc5358b535985691686ea2
Chanjung Kim <freiyer.paxbun@gmail.com>
2022-04-27 10:39:58 +0900

    Add argument value random generation in main.cxx

--- jitdemo.shell/main.cxx
+++ jitdemo.shell/main.cxx
@@ -4,7 +4,9 @@
 #include <cmath>
 #include <iomanip>
 #include <iostream>
+#include <random>
 #include <span>
+#include <string>
 #include <vector>
 
 import jitdemo.expr;
@@ -18,29 +20,42 @@ using ::jitdemo::jit::Compile;
 int main(int argc, char** argv)
 {
     Context context;
-    // auto    source { u8"f(x, y) = x^3 / x * sin(x) + .5 * log(x, 10.2) -  y   " };
-    auto source { u8"f(x, y) = (x * 2 - 3 / y * y) * x" };
+
+    ::std::string sourceNonUtf8;
+    ::std::getline(::std::cin, sourceNonUtf8);
+
+    ::std::u8string_view source {
+        reinterpret_cast<char8_t*>(sourceNonUtf8.data()),
+        reinterpret_cast<char8_t*>(sourceNonUtf8.data() + sourceNonUtf8.size()),
+    };
+
     auto result { Tokenize(source) };
     auto parseResult { Parse(context, result.tokens) };
-
-    double params[] { 1123.3, 4.128973 };
-
-    {
-        double value {
-            ::std::pow(params[0], 3) / params[0] * ::std::sin(params[0])
-                + .5 * ::std::log(10.2) / ::std::log(params[0]) - params[1],
-        };
-        ::std::cout << "Expected: " << ::std::setprecision(8) << value << '\n';
-    }
-
     auto exprTreeFunction { parseResult.function };
+    auto compiledFunction { Compile(exprTreeFunction) };
+
+    ::std::random_device               randomDevice {};
+    ::std::default_random_engine       engine { randomDevice() };
+    ::std::normal_distribution<double> distribution {};
+
+    ::std::vector<double> arguments;
+    ::std::cout << "Arguments:\n";
+    for (auto& param : exprTreeFunction->params())
+    {
+        double argument { distribution(engine) };
+        ::std::cout << "  " << reinterpret_cast<const char*>(param.c_str());
+        ::std::cout << " = " << argument << '\n';
+        arguments.push_back(argument);
+    }
+    ::std::cout << '\n';
+
     {
         ::std::chrono::high_resolution_clock clock {};
 
         auto start { clock.now() };
         for (int i {}; i < 1'000'000; ++i)
         {
-            double value { exprTreeFunction->Evaluate(params) };
+            double value { exprTreeFunction->Evaluate(arguments) };
         }
         auto end { clock.now() };
 
@@ -49,18 +64,17 @@ int main(int argc, char** argv)
         };
         ::std::cout << seconds << " s\n";
 
-        double value { exprTreeFunction->Evaluate(params) };
+        double value { exprTreeFunction->Evaluate(arguments) };
         ::std::cout << "Actual (ExprTree): " << std::setprecision(8) << value << '\n';
     }
 
-    auto compiledFunction { Compile(exprTreeFunction) };
     {
         ::std::chrono::high_resolution_clock clock {};
 
         auto start { clock.now() };
         for (int i {}; i < 1'000'000; ++i)
         {
-            double value { compiledFunction->Evaluate(params) };
+            double value { compiledFunction->Evaluate(arguments) };
         }
         auto end { clock.now() };
 
@@ -69,7 +83,7 @@ int main(int argc, char** argv)
         };
         ::std::cout << seconds << " s\n";
 
-        double value { compiledFunction->Evaluate(params) };
+        double value { compiledFunction->Evaluate(arguments) };
         ::std::cout << "Actual (Compiled): " << std::setprecision(8) << value << '\n';
     }
 
